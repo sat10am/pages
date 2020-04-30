0f3487e2c2eda7e1d97ed6e482c70a7d58e2a12b
DoonDoony <cream.doondoon@gmail.com>
2020-05-01 03:42:33 +0900

    :alien: Fix API url paths

--- api/index.py
+++ api/index.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
+from starlette.responses import JSONResponse
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/api")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/welcome")
+@app.get("/api/openapi.json")
+async def get_open_api_endpoint():
+    return JSONResponse(
+        get_openapi(title="FastAPI", routes=app.routes, version="2")
+    )
+
+
+@app.get("/api/docs")
+async def get_documentation():
+    return get_swagger_ui_html(openapi_url="/api/openapi.json", title="docs")
+
+
+@app.get("/api/welcome")
 async def welcome(name: str):
     return {"message": f"Hello {name.capitalize()}"}
